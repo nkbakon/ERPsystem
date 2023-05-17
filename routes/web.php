f1fbcdbc679786ab2782a999d07838cdd6ad4302
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2023-05-17 10:27:38 +0530

    added middleware

--- routes/web.php
+++ routes/web.php
@@ -26,27 +26,31 @@ Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('/', [AuthController::class, 'loginPost'])->name('login.post');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-Route::resource('customers', CustomerController::class);
-Route::resource('items', ItemController::class);
-Route::resource('invoices', InvoiceController::class);
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::resource('customers', CustomerController::class);
+    Route::resource('items', ItemController::class);
+    Route::resource('invoices', InvoiceController::class);
 
-Route::resource('details', DetailController::class)->except(['update', 'destroy']);
-Route::put('details/district', [DetailController::class, 'districtUpdate'])->name('details.districtUpdate');
-Route::delete('details/district', [DetailController::class, 'districtDestroy'])->name('details.districtDestroy');
+    Route::resource('details', DetailController::class)->except(['update', 'destroy']);
+    Route::put('details/district', [DetailController::class, 'districtUpdate'])->name('details.districtUpdate');
+    Route::delete('details/district', [DetailController::class, 'districtDestroy'])->name('details.districtDestroy');
 
-Route::resource('itemdetails', ItemdetailsController::class)->except(['update', 'destroy']);
-Route::put('itemdetails/district', [ItemdetailsController::class, 'categoryUpdate'])->name('itemdetails.categoryUpdate');
-Route::delete('itemdetails/district', [ItemdetailsController::class, 'categoryDestroy'])->name('itemdetails.categoryDestroy');
+    Route::resource('itemdetails', ItemdetailsController::class)->except(['update', 'destroy']);
+    Route::put('itemdetails/district', [ItemdetailsController::class, 'categoryUpdate'])->name('itemdetails.categoryUpdate');
+    Route::delete('itemdetails/district', [ItemdetailsController::class, 'categoryDestroy'])->name('itemdetails.categoryDestroy');
 
-Route::post('itemdetails/shape', [ItemdetailsController::class, 'subcategory'])->name('itemdetails.subcategory');
-Route::put('itemdetails/shape', [ItemdetailsController::class, 'subcategoryUpdate'])->name('itemdetails.subcategoryUpdate');
-Route::delete('itemdetails/shape', [ItemdetailsController::class, 'subcategoryDestroy'])->name('itemdetails.subcategoryDestroy');
+    Route::post('itemdetails/shape', [ItemdetailsController::class, 'subcategory'])->name('itemdetails.subcategory');
+    Route::put('itemdetails/shape', [ItemdetailsController::class, 'subcategoryUpdate'])->name('itemdetails.subcategoryUpdate');
+    Route::delete('itemdetails/shape', [ItemdetailsController::class, 'subcategoryDestroy'])->name('itemdetails.subcategoryDestroy');
 
-Route::resource('users', UserController::class);
-Route::resource('reports', ReportController::class);
+    Route::group(['middleware' => ['admin']], function() {    
+        Route::resource('users', UserController::class);
+        Route::resource('reports', ReportController::class);
+    });
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', function () {
